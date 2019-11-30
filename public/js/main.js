2df6fdf1794d138f4fe226a55f621460b30fb5e3
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2019-11-30 02:47:07 -0500

    fixes and js done

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,36 @@
 (function (){
 // console.log('wrokin');
 let burger = document.querySelector('.fa-bars'),
-    mainNav = document.querySelector('.main-nav');
+    mainNav = document.querySelector('.main-nav'),
+    mediaQuery1 = window.matchMedia('(min-width: 1024px)'),
+    mediaQuery2 = window.matchMedia('(min-width: 768px)'),
+    linkDev = document.querySelector('#link-dev');
+
+if (mediaQuery1.matches) {
+    console.log('helloe');
+    mainNav.classList.remove('visible');
+}
 
 function expandNav() {
     burger.classList.toggle('rotated');
     mainNav.classList.toggle('visible');
 }
 
+function scrollToDevelopment(e){
+	e.preventDefault;
+	if (mediaQuery1.matches) {
+		TweenLite.to(window, 1, {scrollTo:{y:700}});
+	}
+	else if (mediaQuery2.matches) {
+		TweenLite.to(window, 1, {scrollTo:{y:650}});
+	}
+	else {
+		TweenLite.to(window, 1, {scrollTo:{y:700}})
+	}
+}
+
+
 burger.addEventListener('click', expandNav);
+linkDev.addEventListener('click', scrollToDevelopment);
 
 })();
